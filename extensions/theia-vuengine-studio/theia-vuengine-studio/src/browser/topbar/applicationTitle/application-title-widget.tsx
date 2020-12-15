baa2aed0eca3b6ff0245b0a762a57f428abd6971
Christian Radke <christian.radke@unicepta.com>
2020-12-15 09:46:52 +0100

    Improve topbar media queries

--- extensions/theia-vuengine-studio/theia-vuengine-studio/src/browser/topbar/applicationTitle/application-title-widget.tsx
+++ extensions/theia-vuengine-studio/theia-vuengine-studio/src/browser/topbar/applicationTitle/application-title-widget.tsx
@@ -1,27 +1,26 @@
-import * as React from 'react';
-import { injectable, postConstruct } from 'inversify';
-import { ReactWidget } from '@theia/core/lib/browser/widgets/react-widget';
-import { FrontendApplicationConfigProvider } from '@theia/core/lib/browser/frontend-application-config-provider';
+import * as React from "react";
+import { injectable, postConstruct } from "inversify";
+import { ReactWidget } from "@theia/core/lib/browser/widgets/react-widget";
+import { FrontendApplicationConfigProvider } from "@theia/core/lib/browser/frontend-application-config-provider";
+import { getOs } from "../../common";
 
 @injectable()
 export class VesTopbarApplicationTitleWidget extends ReactWidget {
+  static readonly ID = "ves-topbar-application-title";
+  static readonly LABEL = "Topbar Action Buttons";
 
-    static readonly ID = 'ves-topbar-application-title';
-    static readonly LABEL = 'Topbar Action Buttons';
+  @postConstruct()
+  protected async init(): Promise<void> {
+    this.id = VesTopbarApplicationTitleWidget.ID;
+    this.title.label = VesTopbarApplicationTitleWidget.LABEL;
+    this.title.caption = VesTopbarApplicationTitleWidget.LABEL;
+    this.title.closable = false;
+    this.addClass(`os-${getOs()}`);
+    this.update();
+  }
 
-    @postConstruct()
-    protected async init(): Promise<void> {
-        this.id = VesTopbarApplicationTitleWidget.ID;
-        this.title.label = VesTopbarApplicationTitleWidget.LABEL;
-        this.title.caption = VesTopbarApplicationTitleWidget.LABEL;
-        this.title.closable = false;
-        this.update();
-    }
-
-    protected render(): React.ReactNode {
-        const { applicationName } = FrontendApplicationConfigProvider.get();
-        return <>
-            {applicationName}
-        </>;
-    }
+  protected render(): React.ReactNode {
+    const { applicationName } = FrontendApplicationConfigProvider.get();
+    return <>{applicationName}</>;
+  }
 }
--- extensions/theia-vuengine-studio/theia-vuengine-studio/src/browser/topbar/windowControls/window-controls-widget.tsx
+++ extensions/theia-vuengine-studio/theia-vuengine-studio/src/browser/topbar/windowControls/window-controls-widget.tsx
@@ -1,70 +1,83 @@
-import * as electron from 'electron';
-import * as React from 'react';
-import { inject, injectable, postConstruct } from 'inversify';
-import { ReactWidget } from '@theia/core/lib/browser/widgets/react-widget';
-import { ElectronCommands } from '@theia/core/lib/electron-browser/menu/electron-menu-contribution';
-import { CommandService } from '@theia/core';
-import { VesMaximizeWindowCommand, VesMinimizeWindowCommand, VesUnmaximizeWindowCommand } from './commands';
+import * as electron from "electron";
+import * as React from "react";
+import { inject, injectable, postConstruct } from "inversify";
+import { ReactWidget } from "@theia/core/lib/browser/widgets/react-widget";
+import { ElectronCommands } from "@theia/core/lib/electron-browser/menu/electron-menu-contribution";
+import { CommandService } from "@theia/core";
+import {
+  VesMaximizeWindowCommand,
+  VesMinimizeWindowCommand,
+  VesUnmaximizeWindowCommand,
+} from "./commands";
 
 @injectable()
 export class VesTopbarWindowControlsWidget extends ReactWidget {
+  static readonly ID = "ves-topbar-window-controls";
+  static readonly LABEL = "Topbar Window Controls";
 
-    static readonly ID = 'ves-topbar-window-controls';
-    static readonly LABEL = 'Topbar Window Controls';
+  @inject(CommandService)
+  protected readonly commandService!: CommandService;
 
-    @inject(CommandService)
-    protected readonly commandService!: CommandService;
+  @postConstruct()
+  protected async init(): Promise<void> {
+    this.id = VesTopbarWindowControlsWidget.ID;
+    this.title.label = VesTopbarWindowControlsWidget.LABEL;
+    this.title.caption = VesTopbarWindowControlsWidget.LABEL;
+    this.title.closable = false;
+    electron.remote.getCurrentWindow().on("maximize", () => this.update());
+    electron.remote.getCurrentWindow().on("unmaximize", () => this.update());
+    this.update();
+  }
 
-    @postConstruct()
-    protected async init(): Promise<void> {
-        this.id = VesTopbarWindowControlsWidget.ID;
-        this.title.label = VesTopbarWindowControlsWidget.LABEL;
-        this.title.caption = VesTopbarWindowControlsWidget.LABEL;
-        this.title.closable = false;
-        electron.remote.getCurrentWindow().on("maximize", () => this.update());
-        electron.remote.getCurrentWindow().on("unmaximize", () => this.update());
-        this.update();
-    }
-
-    protected render(): React.ReactNode {
-        return <>
-            <div className="topbar-window-controls-separator"></div>
-            <div
-                className="topbar-window-controls-button"
-                id="ves-topbar-window-controls-minimize"
-                onClick={() => this.commandService.executeCommand(VesMinimizeWindowCommand.id)}
-            >
-                ‒
-            </div>
-            {!this.isMaximized() &&
-                <div
-                    className="topbar-window-controls-button"
-                    id="ves-topbar-window-controls-maximize"
-                    onClick={() => this.commandService.executeCommand(VesMaximizeWindowCommand.id)}
-                >
-                    ◻
-                </div>
+  protected render(): React.ReactNode {
+    return (
+      <>
+        <div className="topbar-window-controls-separator"></div>
+        <div
+          className="topbar-window-controls-button"
+          id="ves-topbar-window-controls-minimize"
+          onClick={() =>
+            this.commandService.executeCommand(VesMinimizeWindowCommand.id)
+          }
+        >
+          ‒
+        </div>
+        {!this.isMaximized() && (
+          <div
+            className="topbar-window-controls-button"
+            id="ves-topbar-window-controls-maximize"
+            onClick={() =>
+              this.commandService.executeCommand(VesMaximizeWindowCommand.id)
             }
-            {this.isMaximized() &&
-                <div
-                    className="topbar-window-controls-button"
-                    id="ves-topbar-window-controls-restore"
-                    onClick={() => this.commandService.executeCommand(VesUnmaximizeWindowCommand.id)}
-                >
-                    ❐
-                </div>
+          >
+            ◻
+          </div>
+        )}
+        {this.isMaximized() && (
+          <div
+            className="topbar-window-controls-button"
+            id="ves-topbar-window-controls-restore"
+            onClick={() =>
+              this.commandService.executeCommand(VesUnmaximizeWindowCommand.id)
             }
-            <div
-                className="topbar-window-controls-button"
-                id="ves-topbar-window-controls-close"
-                onClick={() => this.commandService.executeCommand(ElectronCommands.CLOSE_WINDOW.id)}
-            >
-                ⨉
-            </div>
-        </>;
-    }
+          >
+            ❐
+          </div>
+        )}
+        <div
+          className="topbar-window-controls-button"
+          id="ves-topbar-window-controls-close"
+          onClick={() =>
+            this.commandService.executeCommand(ElectronCommands.CLOSE_WINDOW.id)
+          }
+        >
+          ⨉
+        </div>
+      </>
+    );
+  }
 
-    protected isMaximized(): boolean {
-        return electron.remote.getCurrentWindow().isMaximized();
-    }
+  protected isMaximized(): boolean {
+    return electron.remote.getCurrentWindow().isMaximized();
+  }
 }
