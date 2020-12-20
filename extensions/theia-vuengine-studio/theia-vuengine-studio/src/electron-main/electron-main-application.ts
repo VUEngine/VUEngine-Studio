076e9fad36cd5e4dd93e9aa308e229ea87f94aba
Christian Radke <chris@vr32.de>
2020-12-20 23:40:35 +0100

    Rework theme

--- extensions/theia-vuengine-studio/theia-vuengine-studio/src/electron-main/electron-main-application.ts
+++ extensions/theia-vuengine-studio/theia-vuengine-studio/src/electron-main/electron-main-application.ts
@@ -1,16 +1,24 @@
 import { injectable } from 'inversify';
+import { BrowserWindow } from 'electron';
 import { ElectronMainApplication, TheiaBrowserWindowOptions } from '@theia/core/lib/electron-main/electron-main-application';
-import { isOSX } from '@theia/core';
+import { isOSX, MaybePromise } from '@theia/core';
 
 @injectable()
 export class VesElectronMainApplication extends ElectronMainApplication {
+    async createWindow(asyncOptions: MaybePromise<TheiaBrowserWindowOptions> = this.getDefaultBrowserWindowOptions()): Promise<BrowserWindow> {
+        const electronWindow = await super.createWindow(asyncOptions);
+        // electronWindow.on('focus', () => electronWindow.setOpacity(1));
+        // electronWindow.on('blur', () => electronWindow.setOpacity(0.95));
+        return electronWindow;
+    }
+
     protected async getDefaultBrowserWindowOptions(): Promise<TheiaBrowserWindowOptions> {
         return {
             ...await super.getDefaultBrowserWindowOptions(),
-            frame: isOSX,
-            minWidth: 820,
-            minHeight: 420,
             backgroundColor: "#222",
+            frame: isOSX,
+            minHeight: 420,
+            minWidth: 820,
             titleBarStyle: "hiddenInset"
         }
     }
