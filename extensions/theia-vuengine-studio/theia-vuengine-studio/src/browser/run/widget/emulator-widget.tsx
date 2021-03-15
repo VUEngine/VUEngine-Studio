594673fc25068a3235f379fc420cd51d29abe991
Christian Radke <christian.radke@unicepta.com>
2021-03-15 23:13:23 +0100

    Progress on emulator widget

--- extensions/theia-vuengine-studio/theia-vuengine-studio/src/browser/run/widget/emulator-widget.tsx
+++ extensions/theia-vuengine-studio/theia-vuengine-studio/src/browser/run/widget/emulator-widget.tsx
@@ -2,28 +2,37 @@ import * as React from "react";
 import { join as joinPath } from "path";
 import { inject, injectable, postConstruct } from "inversify";
 import { ReactWidget } from "@theia/core/lib/browser/widgets/react-widget";
-import { FrontendApplicationState, FrontendApplicationStateService } from "@theia/core/lib/browser/frontend-application-state";
+import {
+  FrontendApplicationState,
+  FrontendApplicationStateService,
+} from "@theia/core/lib/browser/frontend-application-state";
 import { getResourcesPath, getWorkspaceRoot } from "../../common/functions";
 
-const datauri = require('datauri');
+const datauri = require("datauri");
+
+// TODO: when emulator tab is being switched to, focus emulator
 
 @injectable()
 export class VesEmulatorWidget extends ReactWidget {
-  @inject(FrontendApplicationStateService) protected readonly frontendApplicationStateService: FrontendApplicationStateService;
+  @inject(FrontendApplicationStateService)
+  protected readonly frontendApplicationStateService: FrontendApplicationStateService;
 
   static readonly ID = "vesEmulatorWidget";
   static readonly LABEL = "Emulator";
 
+  static readonly RESOLUTIONX = 384;
+  static readonly RESOLUTIONY = 224;
+
   protected wrapperRef = React.createRef<HTMLDivElement>();
-  protected canvasRef = React.createRef<HTMLIFrameElement>();
 
   protected state = {
     paused: false,
     coreOptions: {
-      stereoMode: "anaglyph-red-blue",
-      colorMode: "black-red",
-    }
-  }
+      emulationMode: "accurate",
+      stereoMode: "2d-black-red",
+    },
+    canvasScaleFactor: 1,
+  };
 
   @postConstruct()
   protected async init(): Promise<void> {
@@ -35,139 +44,364 @@ export class VesEmulatorWidget extends ReactWidget {
 
     this.update();
 
-    this.frontendApplicationStateService.onStateChanged(async (state: FrontendApplicationState) => {
-      if (state === 'ready') {
-        const romPath = joinPath(getWorkspaceRoot(), "build", "output.vb");
-        datauri(romPath, (err: any) => {
-          if (err) throw err;
-        }).then((content: string) => {
-          this.sendCoreOptions();
-          this.sendCommand("start", content);
-        });
+    this.frontendApplicationStateService.onStateChanged(
+      async (state: FrontendApplicationState) => {
+        if (state === "ready") {
+          const romPath = joinPath(getWorkspaceRoot(), "build", "output.vb");
+          datauri(romPath, (err: any) => {
+            if (err) throw err;
+          }).then((content: string) => {
+            this.determineScalingFactor();
+            this.sendCoreOptions();
+            this.sendCommand("start", content);
+            this.update();
+          });
+        }
       }
-    });
+    );
+  }
+
+  protected onResize() {
+    this.determineScalingFactor();
+    this.update();
   }
 
   protected render(): React.ReactNode {
-    return <>
-      <div id="vesEmulatorUi">
-        <div>
-          {/**/
-            <button className="theia-button secondary" title="Clean" onClick={() => this.sendCommand("clean")}>
-              <i className="fa fa-trash"></i>
+    return (
+      <>
+        <div id="vesEmulatorUi">
+          <div>
+            {/*/
+              <button
+                className="theia-button secondary"
+                title="Clean"
+                onClick={() => this.sendCommand("clean")}
+              >
+                <i className="fa fa-trash"></i>
+              </button>
+              /**/}
+            {this.state.paused ? (
+              <button
+                className="theia-button secondary"
+                title="Resume"
+                onClick={() => this.togglePause()}
+              >
+                <i className="fa fa-play"></i>
+              </button>
+            ) : (
+              <button
+                className="theia-button secondary"
+                title="Pause"
+                onClick={() => this.togglePause()}
+              >
+                <i className="fa fa-pause"></i>
+              </button>
+            )}
+            <button
+              className="theia-button secondary"
+              title="Rewind"
+              onClick={() => this.sendCommand("rewind")}
+            >
+              <i className="fa fa-backward"></i>
             </button>
-          /**/}
-          {this.state.paused
-            ? <button className="theia-button secondary" title="Resume" onClick={() => this.togglePause()}>
-              <i className="fa fa-play"></i>
+            <button
+              className="theia-button secondary"
+              title="Frame Advance"
+              onClick={() => this.sendCommand("frameAdvance")}
+            >
+              <i className="fa fa-step-forward"></i>
             </button>
-            : <button className="theia-button secondary" title="Pause" onClick={() => this.togglePause()}>
-              <i className="fa fa-pause"></i>
+
+            <button
+              className="theia-button secondary"
+              title="Toggle Fast Forward"
+              onClick={() => this.sendCommand("toggleFastForward")}
+            >
+              <i className="fa fa-forward"></i>
             </button>
-          }
-          <button className="theia-button secondary" title="Screenshot" onClick={() => this.sendCommand("screenshot")}>
-            <i className="fa fa-camera"></i>
-          </button>
-          <button className="theia-button secondary" title="Save State" onClick={() => this.sendCommand("saveState")}>
-            <i className="fa fa-level-down"></i> <i className="fa fa-bookmark-o"></i>
-          </button>
-          <button className="theia-button secondary" title="Load State" onClick={() => this.sendCommand("loadState")}>
-            <i className="fa fa-bookmark-o"></i> <i className="fa fa-level-up"></i>
-          </button>
+            <button
+              className="theia-button secondary"
+              title="Reset"
+              onClick={() => this.sendCommand("reset")}
+            >
+              <i className="fa fa-refresh"></i>
+            </button>
+          </div>
+          <div>
+            <button
+              className="theia-button secondary"
+              title="Volume Up"
+              onClick={() => this.sendCommand("volumeUp")}
+            >
+              <i className="fa fa-volume-up"></i>
+            </button>
+            <button
+              className="theia-button secondary"
+              title="Volume Down"
+              onClick={() => this.sendCommand("volumeDown")}
+            >
+              <i className="fa fa-volume-down"></i>
+            </button>
+            <button
+              className="theia-button secondary"
+              title="Mute"
+              onClick={() => this.sendCommand("mute")}
+            >
+              <i className="fa fa-volume-off"></i>
+            </button>
+          </div>
+          <div>
+            <button
+              className="theia-button secondary"
+              title="Increase Save State Slot"
+              onClick={() => this.sendCommand("increaseSaveSlot")}
+            >
+              <i className="fa fa-chevron-up"></i>
+            </button>
+            <button
+              className="theia-button secondary"
+              title="Decrease Save State Slot"
+              onClick={() => this.sendCommand("decreaseSaveSlot")}
+            >
+              <i className="fa fa-chevron-down"></i>
+            </button>
+            <button
+              className="theia-button secondary"
+              title="Save State"
+              onClick={() => this.sendCommand("saveState")}
+            >
+              <i className="fa fa-level-down"></i>{" "}
+              <i className="fa fa-bookmark-o"></i>
+            </button>
+            <button
+              className="theia-button secondary"
+              title="Load State"
+              onClick={() => this.sendCommand("loadState")}
+            >
+              <i className="fa fa-bookmark-o"></i>{" "}
+              <i className="fa fa-level-up"></i>
+            </button>
+          </div>
+          <div>
+            <select className="theia-select" title="Scale">
+              <option value="auto">Auto scale</option>
+              <option value="1">×1</option>
+              <option value="2">×2</option>
+              <option value="3">×3</option>
+              <option value="4">×4</option>
+              <option value="5">×5</option>
+              <option value="full">Full width</option>
+            </select>
+            <button
+              className="theia-button secondary"
+              title="Fullscreen"
+              onClick={() => this.sendCommand("fullscreen")}
+            >
+              <i className="fa fa-arrows-alt"></i>
+            </button>
+          </div>
+          <div>
+            <select
+              className="theia-select"
+              title="3D Mode"
+              onChange={(value) => this.setStereoMode(value)}
+            >
+              <option value="2d-black-red">2D (Red/Black)</option>
+              <option value="2d-black-white">2D (White/Black)</option>
+              <option value="2d-black-blue">2D (Blue/Black)</option>
+              <option value="2d-black-cyan">2D (Cyan/Black)</option>
+              <option value="2d-black-electric-cyan">
+                2D (Electric Cyan/Black)
+              </option>
+              <option value="2d-black-green">2D (Green/Black)</option>
+              <option value="2d-black-magenta">2D (Magenta/Black)</option>
+              <option value="2d-black-yellow">2D (Yellow/Black)</option>
+
+              <option value="anaglyph-red-blue">Anaglyph (Red/Blue)</option>
+              <option value="anaglyph-red-cyan">Anaglyph (Red/Cyan)</option>
+              <option value="anaglyph-red-electric-cyan">
+                Anaglyph (Red/Electric Cyan)
+              </option>
+              {/* TODO: Mednafen's red/green option does not seem to exist in Beetle VB */}
+              <option value="anaglyph-red-green">Anaglyph (Red/Green)</option>
+              <option value="anaglyph-green-magenta">
+                Anaglyph (Green/Magenta)
+              </option>
+              <option value="anaglyph-yellow-blue">
+                Anaglyph (Yellow/Blue)
+              </option>
+              <option value="side-by-side">Side-by-Side</option>
+              <option value="cyberscope">CyberScope</option>
+              <option value="hli">Horizontal Line Interlaced</option>
+              <option value="vli">Vertical Line Interlaced</option>
+            </select>
+            <select
+              className="theia-select"
+              title="Emulation Mode"
+              onChange={(value) => this.setEmulationMode(value)}
+            >
+              <option value="accurate">Accurate</option>
+              <option value="fast">Fast</option>
+            </select>
+          </div>
+          <div>
+            <button
+              className="theia-button secondary"
+              title="Screenshot"
+              onClick={() => this.sendCommand("screenshot")}
+            >
+              <i className="fa fa-camera"></i>
+            </button>
+            <button
+              className="theia-button secondary"
+              title="Menu"
+              onClick={() => this.sendCommand("menu")}
+            >
+              <i className="fa fa-bars"></i>
+            </button>
+          </div>
+          <div>
+            <button className="theia-button secondary" title="Configure Input">
+              <i className="fa fa-gamepad"></i>
+            </button>
+          </div>
         </div>
-        <div>
-          <button className="theia-button secondary" title="Set Scale 1" onClick={() => this.sendCommand("scale1")}>
-            ×1
-        </button>
-          <button className="theia-button secondary" title="Set Scale 2" onClick={() => this.sendCommand("scale2")}>
-            ×2
-          </button>
-          <button className="theia-button secondary" title="Set Scale 3" onClick={() => this.sendCommand("scale3")}>
-            ×3
-          </button>
-          <button className="theia-button secondary" title="Set Full Width" onClick={() => this.sendCommand("scale10")}>
-            Full
-          </button>
-          <button className="theia-button secondary" title="Fullscreen" onClick={() => this.sendCommand("fullscreen")}>
-            <i className="fa fa-arrows-alt"></i>
-          </button>
+        <div id="vesEmulatorWrapper" ref={this.wrapperRef}>
+          {this.state.paused && (
+            <div className="pauseOverlay">
+              <i className="fa fa-pause fa-4x"></i>
+            </div>
+          )}
+          <iframe
+            id="vesEmulatorFrame"
+            src={this.getResoure()}
+            width={this.getCanvasDimensions().width}
+            height={this.getCanvasDimensions().height}
+          ></iframe>
         </div>
-        <div>
-          <select className="theia-select" title="3D Mode" onClick={(value) => this.setStereoMode(value)}>
-            <option value="off">No 3-D</option>
-            <option value="anaglyph-red-blue">Anaglyph (Red/Blue)</option>
-            <option value="anaglyph-red-cyan">Anaglyph (Red/Cyan)</option>
-            <option value="anaglyph-red-electric-cyan">Anaglyph (Red/Electric Cyan)</option>
-            {/* TODO: Mednafen's red/green option does not seem to exist in Beetle VB */}
-            <option value="anaglyph-red-green">Anaglyph (Red/Green)</option>
-            <option value="anaglyph-green-magenta">Anaglyph (Green/Magenta)</option>
-            <option value="anaglyph-yellow-blue">Anaglyph (Yellow/Blue)</option>
-            <option value="side-by-side">Side-by-Side</option>
-            <option value="cyberscope">CyberScope</option>
-            <option value="vli">Horizontal Line Interlaced</option>
-            <option value="hli">Vertical Line Interlaced</option>
-          </select>
-        </div>
-        <div>
-          <button className="theia-button secondary" title="Menu" onClick={() => this.sendCommand("menu")}>
-            <i className="fa fa-bars"></i>
-          </button>
-        </div>
-      </div>
-      <div id="vesEmulatorWrapper" ref={this.wrapperRef}>
-        {this.state.paused &&
-          <div className="pauseOverlay"><i className="fa fa-pause fa-4x"></i></div>
-        }
-        <iframe id="vesEmulatorFrame" src={this.getResoure()} ref={this.canvasRef}></iframe>
-      </div>
-    </>;
+      </>
+    );
   }
 
   protected getResoure() {
-    return joinPath(getResourcesPath(), "binaries", "vuengine-studio-tools", "retroarch", "index.html");
+    return joinPath(
+      getResourcesPath(),
+      "binaries",
+      "vuengine-studio-tools",
+      "retroarch",
+      "index.html"
+    );
   }
 
   protected sendCommand(command: string, data?: any) {
     // @ts-ignore
-    document.getElementById("vesEmulatorFrame").contentWindow.postMessage({ command, data });
+    document
+      .getElementById("vesEmulatorFrame")
+      // @ts-ignore
+      .contentWindow.postMessage({ command, data });
   }
 
   protected async togglePause() {
     if (this.state.paused) {
-      this.sendCommand("resume")
+      this.sendCommand("resume");
     } else {
-      this.sendCommand("pause")
+      this.sendCommand("pause");
     }
     this.state.paused = !this.state.paused;
     this.update();
   }
 
-  protected setStereoMode(e: any/*: React.MouseEvent<Element, MouseEvent>*/) {
-    // this.state.coreOptions.stereoMode = evt. value;
+  protected setEmulationMode(e: React.ChangeEvent<HTMLSelectElement>) {
+    this.state.coreOptions.emulationMode = e.target.value;
+    this.sendCoreOptions();
+  }
+
+  protected setStereoMode(e: React.ChangeEvent<HTMLSelectElement>) {
+    this.state.coreOptions.stereoMode = e.target.value;
     this.sendCoreOptions();
   }
 
   protected sendCoreOptions() {
+    const emulationMode = this.state.coreOptions.emulationMode;
     let stereoMode = this.state.coreOptions.stereoMode;
-    let anaglyphPreset = "disabled"
-    let colorMode = this.state.coreOptions.colorMode.replace("-", " & ").replace("-", " ")
+    let anaglyphPreset = "disabled";
+    let colorMode = "black & red";
 
-    if (stereoMode === "off") {
-      anaglyphPreset = "disabled"
-      stereoMode = "anaglyph"
+    if (stereoMode.startsWith("2d")) {
+      colorMode = stereoMode
+        .substr(3)
+        .replace("-", " & ")
+        .replace("-", " ");
+      anaglyphPreset = "disabled";
+      stereoMode = "anaglyph";
     } else if (stereoMode.startsWith("anaglyph")) {
-      anaglyphPreset = stereoMode.substr(9).replace("-", " & ").replace("-", " ")
-      stereoMode = "anaglyph"
+      anaglyphPreset = stereoMode
+        .substr(9)
+        .replace("-", " & ")
+        .replace("-", " ");
+      stereoMode = "anaglyph";
     }
 
-    this.sendCommand("setCoreOptions", `
-      vb_3dmode = "${stereoMode}"
-      vb_anaglyph_preset = "${anaglyphPreset}"
-      vb_color_mode = "${colorMode}"
-      vb_right_analog_to_digital = "disabled"
-      vb_cpu_emulation = "accurate"
-    `);
+    this.sendCommand(
+      "setCoreOptions",
+      `
+        vb_3dmode = "${stereoMode}"
+        vb_anaglyph_preset = "${anaglyphPreset}"
+        vb_color_mode = "${colorMode}"
+        vb_right_analog_to_digital = "disabled"
+        vb_cpu_emulation = "${emulationMode}"
+      `
+    );
   }
 
-  // TODO: make canvas in iframe always 100% in size and move size control to this widget
+  protected sendRetroArchConfig() {
+    this.sendCommand(
+      "setRetroArchConfig",
+      `
+      menu_driver = "xmb"
+    `
+    );
+  }
+
+  protected getCanvasDimensions(): { height: number; width: number } {
+    const wrapperHeight =
+      this.wrapperRef.current?.offsetHeight || VesEmulatorWidget.RESOLUTIONY;
+    const wrapperWidth =
+      this.wrapperRef.current?.offsetWidth || VesEmulatorWidget.RESOLUTIONX;
+
+    if (!this.state.canvasScaleFactor) {
+      const fullSizeCanvasScaleFactor = Math.min.apply(Math, [
+        wrapperHeight / VesEmulatorWidget.RESOLUTIONY,
+        wrapperWidth / VesEmulatorWidget.RESOLUTIONX,
+      ]);
+      return {
+        height: fullSizeCanvasScaleFactor * VesEmulatorWidget.RESOLUTIONY,
+        width: fullSizeCanvasScaleFactor * VesEmulatorWidget.RESOLUTIONX,
+      };
+    } else {
+      return {
+        height: this.state.canvasScaleFactor * VesEmulatorWidget.RESOLUTIONY,
+        width: this.state.canvasScaleFactor * VesEmulatorWidget.RESOLUTIONX,
+      };
+    }
+  }
+
+  protected determineScalingFactor() {
+    if (this.state.canvasScaleFactor > 0) {
+      const wrapperHeight =
+        this.wrapperRef.current?.offsetHeight || VesEmulatorWidget.RESOLUTIONY;
+      const wrapperWidth =
+        this.wrapperRef.current?.offsetWidth || VesEmulatorWidget.RESOLUTIONX;
+
+      this.state.canvasScaleFactor = Math.min.apply(Math, [
+        Math.floor(wrapperHeight / VesEmulatorWidget.RESOLUTIONY),
+        Math.floor(wrapperWidth / VesEmulatorWidget.RESOLUTIONX),
+      ]);
+    }
+  }
+
+  protected resizeCanvas(scaleFactor: number) {
+    this.state.canvasScaleFactor = scaleFactor;
+    this.update();
+  }
 }
