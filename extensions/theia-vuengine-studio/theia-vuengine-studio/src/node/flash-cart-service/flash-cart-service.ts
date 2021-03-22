faffe6c95dd35c7ec9fdf732c1baf441b9136116
KR155E <c.radke@posteo.de>
2021-03-23 00:01:49 +0100

    Refactor flash cart detection algorithm to allow multiple of the same cart as well as prevent dupes

--- extensions/theia-vuengine-studio/theia-vuengine-studio/src/node/flash-cart-service/flash-cart-service.ts
+++ extensions/theia-vuengine-studio/theia-vuengine-studio/src/node/flash-cart-service/flash-cart-service.ts
@@ -27,10 +27,18 @@ export class VesFlashCartServiceImpl implements VesFlashCartService {
         const devices: Device[] = getDeviceList();
         let manufacturer: string | undefined;
         let product: string | undefined;
-        for (const flashCartConfig of flashCartConfigs) {
-            for (let i = 0; i < devices.length; i++) {
+        let deviceIsFlashCart = false;
+        // For every connected USB device, check if it is one of the configured flash carts.
+        // This way...
+        // 1) multiple carts of the same type can be used
+        // 2) the same device cannot be detected for two different configurations with the same
+        //    vid, hid, etc, because we continue to the next device when we match a config
+        for (let i = 0; i < devices.length; i++) {
+            deviceIsFlashCart = false;
+            for (const flashCartConfig of flashCartConfigs) {
                 const deviceDesc = devices[i].deviceDescriptor;
                 if (
+                    !deviceIsFlashCart &&
                     deviceDesc.idVendor == flashCartConfig.vid &&
                     deviceDesc.idProduct == flashCartConfig.pid
                 ) {
@@ -38,17 +46,13 @@ export class VesFlashCartServiceImpl implements VesFlashCartService {
                     manufacturer = await new Promise((resolve, reject) => {
                         devices[i].getStringDescriptor(
                             devices[i].deviceDescriptor.iManufacturer,
-                            (error, data) => {
-                                resolve(data);
-                            }
+                            (error, data) => resolve(data)
                         );
                     });
                     product = await new Promise((resolve, reject) => {
                         devices[i].getStringDescriptor(
                             devices[i].deviceDescriptor.iProduct,
-                            (error, data) => {
-                                resolve(data);
-                            }
+                            (error, data) => resolve(data)
                         );
                     });
                     devices[i].close();
@@ -59,6 +63,7 @@ export class VesFlashCartServiceImpl implements VesFlashCartService {
                         (flashCartConfig.product === "" ||
                             product?.includes(flashCartConfig.product))
                     ) {
+                        deviceIsFlashCart = true;
                         connectedFlashCarts.push({
                             config: flashCartConfig,
                             device: devices[i],
