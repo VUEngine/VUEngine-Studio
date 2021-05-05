975f1c325440618123c99c65d127d2e2f75075d5
Christian Radke <christian.radke@unicepta.com>
2021-05-05 11:15:27 +0200

    Rework ProcessService to use RawProcess instead of TerminalService

--- extensions/theia-vuengine-studio/theia-vuengine-studio/src/common/process-service-protocol.ts
+++ extensions/theia-vuengine-studio/theia-vuengine-studio/src/common/process-service-protocol.ts
@@ -1,21 +1,25 @@
 import { JsonRpcServer } from "@theia/core";
-import { IProcessExitEvent, ProcessErrorEvent, TerminalProcessOptions } from "@theia/process/lib/node";
+import {
+  IProcessExitEvent,
+  ProcessErrorEvent,
+  ProcessOptions,
+} from "@theia/process/lib/node";
 
-export const VES_PROCESS_SERVICE_PATH = '/ves/services/process';
-export const VesProcessService = Symbol('VesProcessService');
+export const VES_PROCESS_SERVICE_PATH = "/ves/services/process";
+export const VesProcessService = Symbol("VesProcessService");
 
 export interface VesProcessServiceClient {
-    onData(pId: number, data: string): void;
-    onError(pId: number, event: ProcessErrorEvent): void;
-    onExit(pId: number, event: IProcessExitEvent): void;
-    onClose(pId: number, event: IProcessExitEvent): void;
+  onData(pId: number, data: string): void;
+  onError(pId: number, event: ProcessErrorEvent): void;
+  onExit(pId: number, event: IProcessExitEvent): void;
+  onClose(pId: number, event: IProcessExitEvent): void;
 }
 
-export interface VesProcessService extends JsonRpcServer<VesProcessServiceClient> {
-    launchProcess(options: TerminalProcessOptions): Promise<{
-        terminalProcessId: number,
-        processManagerId: number,
-        processId: number,
-    }>;
-    killProcess(processManagerId: number): boolean;
+export interface VesProcessService
+  extends JsonRpcServer<VesProcessServiceClient> {
+  launchProcess(options: ProcessOptions): Promise<{
+    processManagerId: number;
+    processId: number;
+  }>;
+  killProcess(processManagerId: number): boolean;
 }
--- extensions/theia-vuengine-studio/theia-vuengine-studio/src/node/process-service/process-service.ts
+++ extensions/theia-vuengine-studio/theia-vuengine-studio/src/node/process-service/process-service.ts
@@ -1,66 +1,74 @@
 import { inject, injectable } from "inversify";
-import { IProcessExitEvent, ProcessErrorEvent, ProcessManager, TerminalProcessFactory, TerminalProcessOptions } from '@theia/process/lib/node';
-import { VesProcessService, VesProcessServiceClient } from "../../common/process-service-protocol";
+import {
+  IProcessExitEvent,
+  ProcessErrorEvent,
+  ProcessManager,
+  ProcessOptions,
+  RawProcessFactory,
+} from "@theia/process/lib/node";
+import {
+  VesProcessService,
+  VesProcessServiceClient,
+} from "../../common/process-service-protocol";
 
 @injectable()
 export class VesProcessServiceImpl implements VesProcessService {
-    @inject(TerminalProcessFactory) protected readonly terminalProcessFactory: TerminalProcessFactory;
-    @inject(ProcessManager) protected readonly processManager: ProcessManager;
-    protected client: VesProcessServiceClient | undefined;
+  @inject(RawProcessFactory)
+  protected readonly rawProcessFactory: RawProcessFactory;
+  @inject(ProcessManager) protected readonly processManager: ProcessManager;
+  protected client: VesProcessServiceClient | undefined;
 
-    dispose(): void {
-        throw new Error("Method not implemented.");
+  dispose(): void {
+    throw new Error("Method not implemented.");
+  }
+
+  async setClient(client: VesProcessServiceClient) {
+    this.client = client;
+  }
+
+  async launchProcess(options: ProcessOptions): Promise<{
+    processManagerId: number;
+    processId: number;
+  }> {
+    const rawProcess = this.rawProcessFactory(options);
+    await new Promise((resolve, reject) => {
+      rawProcess.onStart(resolve);
+      rawProcess.onError((error: ProcessErrorEvent) => console.log(error));
+    });
+
+    const processManagerId = this.processManager.register(rawProcess);
+
+    rawProcess.onClose((event: IProcessExitEvent) => {
+      this.client?.onClose(processManagerId, event);
+    });
+
+    rawProcess.onError((event: ProcessErrorEvent) => {
+      this.client?.onError(processManagerId, event);
+    });
+
+    rawProcess.onExit((event: IProcessExitEvent) => {
+      this.client?.onExit(processManagerId, event);
+    });
+
+    rawProcess.outputStream.on("data", (data: string) => {
+      this.client?.onData(processManagerId, data.toString());
+    });
+
+    const process = this.processManager.get(processManagerId);
+
+    return {
+      processManagerId: processManagerId,
+      processId: process?.pid || -1,
+    };
+  }
+
+  killProcess(processManagerId: number): boolean {
+    const process = this.processManager.get(processManagerId);
+    if (!process) {
+      return false;
     }
 
-    async setClient(client: VesProcessServiceClient) {
-        this.client = client;
-    }
-
-    async launchProcess(options: TerminalProcessOptions): Promise<{
-        terminalProcessId: number,
-        processManagerId: number,
-        processId: number,
-    }> {
-        const terminalProcess = this.terminalProcessFactory(options);
-        await new Promise((resolve, reject) => {
-            terminalProcess.onStart(resolve);
-            terminalProcess.onError((error: ProcessErrorEvent) => console.log(error));
-        });
-
-        const processManagerId = this.processManager.register(terminalProcess);
-
-        terminalProcess.onClose((event: IProcessExitEvent) => {
-            this.client?.onClose(processManagerId, event);
-        });
-
-        terminalProcess.onError((event: ProcessErrorEvent) => {
-            this.client?.onError(processManagerId, event);
-        });
-
-        terminalProcess.onExit((event: IProcessExitEvent) => {
-            this.client?.onExit(processManagerId, event);
-        });
-
-        terminalProcess.outputStream.on('data', (data: string) => {
-            this.client?.onData(processManagerId, data.toString());
-        });
-
-        const process = this.processManager.get(processManagerId);
-
-        return {
-            terminalProcessId: terminalProcess.id,
-            processManagerId: processManagerId,
-            processId: process?.pid || -1,
-        };
-    }
-
-    killProcess(processManagerId: number): boolean {
-        const process = this.processManager.get(processManagerId);
-        if (!process) {
-            return false;
-        }
-
-        process.kill("SIGKILL");
-        return true;
-    }
+    process.kill("SIGKILL");
+    return true;
+  }
 }
