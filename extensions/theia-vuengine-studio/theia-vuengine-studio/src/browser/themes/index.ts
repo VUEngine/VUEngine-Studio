9c88e77706b04aa1feff455d78ce5c34e41a14aa
KR155E <c.radke@posteo.de>
2021-07-05 23:44:50 +0200

    Add Virtual Boy high contrast theme

--- extensions/theia-vuengine-studio/theia-vuengine-studio/src/browser/themes/index.ts
+++ extensions/theia-vuengine-studio/theia-vuengine-studio/src/browser/themes/index.ts
@@ -1,15 +1,29 @@
 import { MonacoThemingService } from '@theia/monaco/lib/browser/monaco-theming-service';
 
 MonacoThemingService.register({
-    label: "Light (VUEngine Studio)",
+    label: "Light (default)",
     id: "vuengine-light",
     uiTheme: "vs",
     json: require("../../../src/browser/themes/vuengine-light-color-theme.json")
 });
 
 MonacoThemingService.register({
-    label: "Dark (VUEngine Studio)",
+    label: "Dark (default)",
     id: "vuengine-dark",
     uiTheme: "vs-dark",
     json: require("../../../src/browser/themes/vuengine-dark-color-theme.json")
+});
+
+MonacoThemingService.register({
+    label: "High Contrast (default)",
+    id: "vuengine-hc",
+    uiTheme: "hc-black",
+    json: require("../../../src/browser/themes/vuengine-high-contrast-color-theme.json")
+});
+
+MonacoThemingService.register({
+    label: "Virtual Boy",
+    id: "vuengine-vb",
+    uiTheme: "hc-black",
+    json: require("../../../src/browser/themes/vuengine-virtual-boy-color-theme.json")
 });
