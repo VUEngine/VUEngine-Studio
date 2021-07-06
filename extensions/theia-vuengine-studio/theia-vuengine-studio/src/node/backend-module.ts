1a965a8b36a462b3b78b9c7e81ceb63075e838c8
KR155E <c.radke@posteo.de>
2021-07-06 22:52:56 +0200

    Override main config dir

--- extensions/theia-vuengine-studio/theia-vuengine-studio/src/node/backend-module.ts
+++ extensions/theia-vuengine-studio/theia-vuengine-studio/src/node/backend-module.ts
@@ -1,8 +1,10 @@
 import { ContainerModule } from 'inversify';
 import { bindVesProcessService } from './process-service/process-service-backend-module';
 import { bindVesFlashCartService } from './flash-cart-service/flash-cart-service-backend-module';
+import { bindVesEnvVariablesServer } from './variables-server/variables-server-backend-module';
 
-export default new ContainerModule(bind => {
+export default new ContainerModule((bind, unbind, isBound, rebind) => {
     bindVesProcessService(bind);
     bindVesFlashCartService(bind);
+    bindVesEnvVariablesServer(rebind);
 });
