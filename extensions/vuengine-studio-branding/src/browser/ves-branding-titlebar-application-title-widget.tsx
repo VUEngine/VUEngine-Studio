d41339eef5d8a311a2cfb589a425a4b8dc2e3ad0
KR155E <c.radke@posteo.de>
2021-07-19 00:09:13 +0200

    Attempt to retrieve project name for title bar from configuration file

--- extensions/vuengine-studio-branding/src/browser/ves-branding-titlebar-application-title-widget.tsx
+++ extensions/vuengine-studio-branding/src/browser/ves-branding-titlebar-application-title-widget.tsx
@@ -1,18 +1,24 @@
 import * as React from 'react';
 import { inject, injectable, postConstruct } from 'inversify';
+import { dirname, join as joinPath } from 'path';
 import { remote } from 'electron'; /* eslint-disable-line */
 import { ReactWidget } from '@theia/core/lib/browser/widgets/react-widget';
-import { FrontendApplicationConfigProvider } from '@theia/core/lib/browser/frontend-application-config-provider';
 import { WorkspaceService } from '@theia/workspace/lib/browser';
 import { isOSX, isWindows } from '@theia/core';
+import URI from '@theia/core/lib/common/uri';
+import { FileService } from '@theia/filesystem/lib/browser/file-service';
+import { FrontendApplicationConfigProvider } from '@theia/core/lib/browser/frontend-application-config-provider';
 
 @injectable()
 export class VesTitlebarApplicationTitleWidget extends ReactWidget {
+  @inject(FileService)
+  private readonly fileService: FileService;
   @inject(WorkspaceService)
   private readonly workspaceService: WorkspaceService;
 
   static readonly ID = 'ves-titlebar-application-title';
   static readonly LABEL = 'Titlebar Action Buttons';
+  protected applicationTitle = '';
 
   @postConstruct()
   protected async init(): Promise<void> {
@@ -21,6 +27,24 @@ export class VesTitlebarApplicationTitleWidget extends ReactWidget {
     this.title.caption = VesTitlebarApplicationTitleWidget.LABEL;
     this.title.closable = false;
     this.addClass(`os-${this.getOs()}`);
+    this.applicationTitle = '';
+
+    // Attempt to retrieve project name from configuration file
+    const projectFileUri = new URI(
+      joinPath(
+        this.getWorkspaceRoot(),
+        '.vuengine',
+        'project.json'
+      )
+    );
+    if (await this.fileService.exists(projectFileUri)) {
+      const configFileContents = await this.fileService.readFile(projectFileUri);
+      const projectData = JSON.parse(configFileContents.value.toString());
+      if (projectData.name) {
+        this.applicationTitle = `${projectData.name} (Project)`;
+      }
+    };
+
     this.update();
 
     this.workspaceService.onWorkspaceChanged(() => this.update());
@@ -32,17 +56,21 @@ export class VesTitlebarApplicationTitleWidget extends ReactWidget {
 
   protected render(): React.ReactNode {
     let { applicationName: title } = FrontendApplicationConfigProvider.get();
-    const workspace = this.workspaceService.workspace;
-    if (workspace !== undefined) {
-      // TODO: attempt to retrieve project name from configuration file, use below as fallback
-      if (this.workspaceService.workspace?.isFile) {
-        const workspaceParts = workspace.name.split('.');
-        workspaceParts.pop();
-        title = `${workspaceParts.join('.')} (Workspace)`;
-      } else {
-        title = workspace.name;
+    if (this.applicationTitle === '') {
+      const workspace = this.workspaceService.workspace;
+      if (workspace !== undefined) {
+        if (this.workspaceService.workspace?.isFile) {
+          const workspaceParts = workspace.name.split('.');
+          workspaceParts.pop();
+          title = `${workspaceParts.join('.')} (Workspace)`;
+        } else {
+          title = workspace.name;
+        }
       }
+    } else {
+      title = this.applicationTitle;
     }
+
     return <div onDoubleClick={this.handleDoubleClick}>{title}</div>;
   }
 
@@ -56,5 +84,13 @@ export class VesTitlebarApplicationTitleWidget extends ReactWidget {
     }
     if (win.isMaximized()) { return win.unmaximize(); };
     return win.maximize();
+  }
+
+  getWorkspaceRoot(): string {
+    const substrNum = isWindows ? 2 : 1;
+
+    return window.location.hash.slice(-9) === 'workspace'
+      ? dirname(window.location.hash.substring(substrNum))
+      : window.location.hash.substring(substrNum);
   }
 }
