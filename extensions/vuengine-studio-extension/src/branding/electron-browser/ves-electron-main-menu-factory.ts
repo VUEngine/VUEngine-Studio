fc5d9a2118edfac8607f9d1a2f039399e069d799
KR155E <c.radke@posteo.de>
2021-09-03 09:33:05 +0200

    Fix electron main menu not rendering correctly

---
+++ extensions/vuengine-studio-extension/src/branding/electron-browser/ves-electron-main-menu-factory.ts
@@ -0,0 +1,30 @@
+import { injectable } from '@theia/core/shared/inversify';
+import { ElectronMainMenuFactory } from '@theia/core/lib/electron-browser/menu/electron-main-menu-factory';
+import { isOSX, MAIN_MENU_BAR } from '@theia/core';
+import { remote } from 'electron';
+
+// This fixes a bug in Theia where the main menu would not render on Mac if the MenuBarVisibility preference is set to "compact"
+// TODO: remove once fixed in Theia
+
+@injectable()
+export class VesElectronMainMenuFactory extends ElectronMainMenuFactory {
+    async setMenuBar(): Promise<void> {
+        const createdMenuBar = this.createMenuBar();
+        if (isOSX) {
+            remote.Menu.setApplicationMenu(createdMenuBar);
+        } else {
+            remote.getCurrentWindow().setMenu(createdMenuBar);
+        }
+    }
+
+    createMenuBar(): Electron.Menu | null {
+        const menuModel = this.menuProvider.getMenu(MAIN_MENU_BAR);
+        const template = this.fillMenuTemplate([], menuModel);
+        if (isOSX) {
+            template.unshift(this.createOSXMenu());
+        }
+        const menu = remote.Menu.buildFromTemplate(template);
+        this._menu = menu;
+        return this._menu;
+    }
+}
