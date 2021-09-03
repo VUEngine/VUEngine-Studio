ac8e9dfc37d570a8cc8a0f4c92312c21fab66f2e
KR155E <c.radke@posteo.de>
2021-09-03 13:41:58 +0200

    Fix override name

--- extensions/vuengine-studio-extension/src/branding/browser/ves-branding-debug-contribution.ts
+++ extensions/vuengine-studio-extension/src/branding/browser/ves-branding-debug-contribution.ts
@@ -2,7 +2,7 @@ import { injectable } from '@theia/core/shared/inversify';
 import { DebugFrontendApplicationContribution } from '@theia/debug/lib/browser/debug-frontend-application-contribution';
 
 @injectable()
-export class VesDebugContribution extends DebugFrontendApplicationContribution {
+export class VesDebugFrontendApplicationContribution extends DebugFrontendApplicationContribution {
     // remove all debug features
     async initializeLayout(): Promise<void> { }
     async onStart(): Promise<void> { }
