39284e801c9c9b272108b149d607edd653625bea
KR155E <c.radke@posteo.de>
2021-09-04 22:59:56 +0200

    Rework Virtual Boy theme

--- extensions/vuengine-studio-extension/src/branding/browser/ves-branding-themes.ts
+++ extensions/vuengine-studio-extension/src/branding/browser/ves-branding-themes.ts
@@ -1,29 +1,39 @@
 import { MonacoThemingService } from '@theia/monaco/lib/browser/monaco-theming-service';
 
 MonacoThemingService.register({
-    label: 'Light (VUEngine Studio)',
     id: 'light', // override theia id
+    label: 'Light (VUEngine Studio)',
     uiTheme: 'vs',
-    json: require('../../../src/branding/browser/themes/vuengine-light-color-theme.json')
+    json: require('../../../src/branding/browser/themes/vuengine-light-color-theme.json'),
+    includes: {
+        './dark_theia.json': require('./../../../../../node_modules/@theia/monaco/data/monaco-themes/vscode/light_theia.json')
+    }
 });
 
 MonacoThemingService.register({
-    label: 'Dark (VUEngine Studio)',
     id: 'dark', // override theia id
+    label: 'Dark (VUEngine Studio)',
     uiTheme: 'vs-dark',
-    json: require('../../../src/branding/browser/themes/vuengine-dark-color-theme.json')
+    json: require('../../../src/branding/browser/themes/vuengine-dark-color-theme.json'),
+    includes: {
+        './dark_theia.json': require('./../../../../../node_modules/@theia/monaco/data/monaco-themes/vscode/dark_theia.json')
+    }
 });
 
 MonacoThemingService.register({
-    label: 'High Contrast (VUEngine Studio)',
     id: 'hc-theia', // override theia id
+    label: 'High Contrast (VUEngine Studio)',
     uiTheme: 'hc-black',
-    json: require('../../../src/branding/browser/themes/vuengine-high-contrast-color-theme.json')
+    json: require('../../../src/branding/browser/themes/vuengine-high-contrast-color-theme.json'),
+    includes: {
+        './dark_theia.json': require('./../../../../../node_modules/@theia/monaco/data/monaco-themes/vscode/hc_theia.json')
+    }
 });
 
+// this is implemented through a filter in style/virtual-boy-theme.css
 MonacoThemingService.register({
-    label: 'Virtual Boy',
     id: 'virtual-boy',
+    label: 'Virtual Boy',
     uiTheme: 'hc-black',
     json: require('../../../src/branding/browser/themes/vuengine-virtual-boy-color-theme.json')
 });
