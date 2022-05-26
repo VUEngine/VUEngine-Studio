7f34aa6d29a7fee339627b926379b6a9c7f121d2
KR155E <c.radke@posteo.de>
2022-05-26 22:35:48 +0200

    Rename preference dir

--- extensions/vuengine-studio-extension/src/branding/browser/ves-branding-preference-configurations.ts
+++ extensions/vuengine-studio-extension/src/branding/browser/ves-branding-preference-configurations.ts
@@ -1,7 +1,7 @@
 import { injectable } from '@theia/core/shared/inversify';
 import { PreferenceConfigurations } from '@theia/core/lib/browser/preferences/preference-configurations';
 
-export const VES_PREFERENCE_DIR = '.vuengine-studio';
+export const VES_PREFERENCE_DIR = '.vuengine';
 
 @injectable()
 export class VesPreferenceConfigurations extends PreferenceConfigurations {
