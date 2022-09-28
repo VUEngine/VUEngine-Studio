3d81b6e7efe9f04771d0750153846bd286fcdaa7
KR155E <c.radke@posteo.de>
2022-09-28 09:58:26 +0200

    Restyle checkboxes

--- extensions/vuengine-studio-extension/src/music-editor/browser/components/Sidebar/CurrentChannel.tsx
+++ extensions/vuengine-studio-extension/src/music-editor/browser/components/Sidebar/CurrentChannel.tsx
@@ -1,3 +1,4 @@
+import { SelectComponent } from '@theia/core/lib/browser/widgets/select-component';
 import React from 'react';
 import HContainer from '../../../../core/browser/components/HContainer';
 import VContainer from '../../../../core/browser/components/VContainer';
@@ -13,16 +14,28 @@ export default function CurrentChannel(props: CurrentChannelProps): JSX.Element 
 
     return <div className='section currentChannel'>
         <VContainer>
-            <label>Channel: {channel.id + 1}</label>
+            <label>Channel</label>
+            <SelectComponent
+                options={[...Array(6)].map((n, i) => ({
+                    value: i.toString(),
+                    label: (i + 1).toString()
+                }))}
+                defaultValue={CurrentChannel.toString()}
+                onChange={option => stateApi.setCurrentChannel(parseInt(option.value!))}
+            />
         </VContainer>
 
         <VContainer>
             Instrument
-            <select
-                className='theia-select'
-            >
-                <option value={0}>Synth</option>
-            </select>
+            <SelectComponent
+                options={[
+                    { value: '0', label: 'Piano' },
+                    { value: '1', label: 'Guitar' },
+                    { value: '2', label: 'Bass' },
+                    { value: '3', label: 'Oboe' },
+                ]}
+                defaultValue={'0'}
+            />
         </VContainer>
 
         <VContainer>
@@ -40,6 +53,33 @@ export default function CurrentChannel(props: CurrentChannelProps): JSX.Element 
                     {channel.volume}
                 </div>
             </HContainer>
+        </VContainer>
+
+        <VContainer>
+            <div>
+                <input
+                    type='checkbox'
+                    checked={channel.collapsed}
+                    onClick={() => stateApi.toggleChannelCollapsed(channel.id)}
+                />
+                <label>Collapsed</label>
+            </div>
+            <div>
+                <input
+                    type='checkbox'
+                    checked={channel.muted}
+                    onClick={() => stateApi.toggleChannelMuted(channel.id)}
+                />
+                <label>Muted</label>
+            </div>
+            <div>
+                <input
+                    type='checkbox'
+                    checked={channel.solo}
+                    onClick={() => stateApi.toggleChannelSolo(channel.id)}
+                />
+                <label>Solo</label>
+            </div>
         </VContainer>
     </div>;
 }
