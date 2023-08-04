92c880681ce227c6811f42030342ffbc97b1ebfe
Christian Radke <c.radke@posteo.de>
2023-08-04 19:13:31 +0200

    Fix user plugins folder

--- applications/electron/scripts/ves-electron-main.js
+++ applications/electron/scripts/ves-electron-main.js
@@ -6,7 +6,7 @@ process.env.THEIA_DEFAULT_PLUGINS = `local-dir:${path.resolve(__dirname, '..', '
 
 // Lookup inside the user's home folder for more plugins, and accept user-defined paths.
 process.env.THEIA_PLUGINS = [
-    process.env.THEIA_PLUGINS, `local-dir:${path.resolve(os.homedir(), '.vuengine-studio', 'plugins')}`,
+    process.env.THEIA_PLUGINS, `local-dir:${path.resolve(os.homedir(), '.vuengine', 'plugins')}`,
 ].filter(Boolean).join(',')
 
 // Handover to the auto-generated electron application handler.
