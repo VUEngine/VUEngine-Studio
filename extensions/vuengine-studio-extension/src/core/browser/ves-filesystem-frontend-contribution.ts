534443572839b93ee98fc6d0589701d4cd170084
Christian Radke <c.radke@posteo.de>
2023-08-05 22:23:45 +0200

    Revert changes related to c_asset, which is no longer a necessary file ending

--- extensions/vuengine-studio-extension/src/core/browser/ves-filesystem-frontend-contribution.ts
+++ extensions/vuengine-studio-extension/src/core/browser/ves-filesystem-frontend-contribution.ts
@@ -1,17 +1,15 @@
 import { injectable } from '@theia/core/shared/inversify';
 import { FileSystemFrontendContribution } from '@theia/filesystem/lib/browser/filesystem-frontend-contribution';
-import { CONVERTED_FILE_ENDING } from '../../image-converter/browser/ves-image-converter-types';
 import { VES_PREFERENCE_DIR } from './ves-preference-configurations';
 
 @injectable()
 export class VesFileSystemFrontendContribution extends FileSystemFrontendContribution {
     protected updateAssociations(): void {
-        const assetCFilePattern = `*.${CONVERTED_FILE_ENDING}`;
         const projectFilePattern = `*${VES_PREFERENCE_DIR}`;
         const fileAssociations: {[filePattern: string]: string} = {
             ...{
-                [assetCFilePattern]: 'c',
                 [projectFilePattern]: 'jsonc',
+                'config.make': 'Makefile',
             },
             ...this.preferences['files.associations'],
         };
