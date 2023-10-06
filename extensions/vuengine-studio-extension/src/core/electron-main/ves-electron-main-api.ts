406278035532053c9b5b3c8326aca126fc9b0efa
Christian Radke <c.radke@posteo.de>
2023-10-06 19:11:44 +0200

    Move some code to backend

--- extensions/vuengine-studio-extension/src/core/electron-main/ves-electron-main-api.ts
+++ extensions/vuengine-studio-extension/src/core/electron-main/ves-electron-main-api.ts
@@ -1,10 +1,12 @@
+import { default as $RefParser, default as JSONSchema } from '@apidevtools/json-schema-ref-parser';
 import { MaybePromise } from '@theia/core';
 import { ElectronMainApplication, ElectronMainApplicationContribution } from '@theia/core/lib/electron-main/electron-main-application';
 import {
     ipcMain, systemPreferences
 } from '@theia/electron/shared/electron';
 import { injectable } from 'inversify';
-import { VES_CHANNEL_GET_USER_DEFAULT, VES_CHANNEL_SET_ZOOM_FACTOR } from '../electron-common/ves-electron-api';
+import sortJson from 'sort-json';
+import { VES_CHANNEL_DEREFERENCE_JSON_SCHEMA, VES_CHANNEL_GET_USER_DEFAULT, VES_CHANNEL_SET_ZOOM_FACTOR, VES_CHANNEL_SORT_JSON } from '../electron-common/ves-electron-api';
 
 @injectable()
 export class VesMainApi implements ElectronMainApplicationContribution {
@@ -14,6 +16,12 @@ export class VesMainApi implements ElectronMainApplicationContribution {
         });
         ipcMain.on(VES_CHANNEL_GET_USER_DEFAULT, (event, preference, type) => {
             event.returnValue = systemPreferences.getUserDefault(preference, type);
+        });
+        ipcMain.on(VES_CHANNEL_DEREFERENCE_JSON_SCHEMA, (event, schema) => {
+            event.returnValue = $RefParser.dereference(schema as JSONSchema);
+        });
+        ipcMain.on(VES_CHANNEL_SORT_JSON, (event, old, options) => {
+            event.returnValue = sortJson(old, options);
         });
     }
 }
