7efc981cb8de0a1de02499917e47dc5e80334956
Christian Radke <c.radke@posteo.de>
2023-10-22 20:58:40 +0200

    Simplify emulator backend

--- extensions/vuengine-studio-extension/src/emulator/node/ves-emulator-backend-module.ts
+++ extensions/vuengine-studio-extension/src/emulator/node/ves-emulator-backend-module.ts
@@ -3,6 +3,5 @@ import { BackendApplicationContribution } from '@theia/core/lib/node';
 import { EmulatorBackendContribution } from './ves-emulator-backend-contribution';
 
 export default new ContainerModule(bind => {
-    bind(EmulatorBackendContribution).toSelf().inSingletonScope();
-    bind(BackendApplicationContribution).toService(EmulatorBackendContribution);
+    bind(BackendApplicationContribution).to(EmulatorBackendContribution).inSingletonScope();
 });
