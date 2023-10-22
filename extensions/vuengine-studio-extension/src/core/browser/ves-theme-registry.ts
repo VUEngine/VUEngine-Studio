b8d89b84ef3da3d1419e96e9b2573cb9dba53913
Christian Radke <c.radke@posteo.de>
2023-10-23 00:51:29 +0200

    Fix theme inheritance

--- extensions/vuengine-studio-extension/src/core/browser/ves-theme-registry.ts
+++ extensions/vuengine-studio-extension/src/core/browser/ves-theme-registry.ts
@@ -4,19 +4,26 @@ import { MonacoThemeRegistry } from '@theia/monaco/lib/browser/textmate/monaco-t
 @injectable()
 export class VesThemeRegistry extends MonacoThemeRegistry {
     initializeDefaultThemes(): void {
-        this.register(require('../../../themes/vuengine-dark-color-theme.json'), {
-            './dark_theia.json': require('../../../../../node_modules/@theia/monaco/data/monaco-themes/vscode/dark_theia.json'),
+        this.register({
+            ...require('../../../../../node_modules/@theia/monaco/data/monaco-themes/vscode/dark_theia.json'),
+            ...require('../../../themes/vuengine-dark-color-theme.json'),
+        }, {
             './dark_vs.json': require('../../../../../node_modules/@theia/monaco/data/monaco-themes/vscode/dark_vs.json'),
             './dark_plus.json': require('../../../../../node_modules/@theia/monaco/data/monaco-themes/vscode/dark_plus.json')
         }, 'dark-vuengine', 'vs-dark');
-        this.register(require('../../../themes/vuengine-light-color-theme.json'), {
-            './light_theia.json': require('../../../../../node_modules/@theia/monaco/data/monaco-themes/vscode/light_theia.json'),
+
+        this.register({
+            ...require('../../../../../node_modules/@theia/monaco/data/monaco-themes/vscode/light_theia.json'),
+            ...require('../../../themes/vuengine-light-color-theme.json'),
+        }, {
             './light_vs.json': require('../../../../../node_modules/@theia/monaco/data/monaco-themes/vscode/light_vs.json'),
-            './light_plus.json': require('../../../../../node_modules/@theia/monaco/data/monaco-themes/vscode/light_plus.json'),
+            './light_plus.json': require('../../../../../node_modules/@theia/monaco/data/monaco-themes/vscode/light_plus.json')
         }, 'light-vuengine', 'vs');
+
         this.register(require('../../../../../node_modules/@theia/monaco/data/monaco-themes/vscode/hc_theia.json'), {
             './hc_black.json': require('../../../../../node_modules/@theia/monaco/data/monaco-themes/vscode/hc_black.json')
         }, 'hc-vuengine', 'hc-black');
+
         this.register(require('../../../../../node_modules/@theia/monaco/data/monaco-themes/vscode/hc_theia_light.json'), {
             './hc_light.json': require('../../../../../node_modules/@theia/monaco/data/monaco-themes/vscode/hc_light.json')
         }, 'hc-vuengine-light', 'hc-light');
