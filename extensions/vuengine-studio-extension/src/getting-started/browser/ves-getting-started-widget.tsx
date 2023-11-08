1fa587311d35c455bbd581cdc32bc320997e3004
Christian Radke <c.radke@posteo.de>
2023-11-08 14:44:26 +0100

    Improve getting started page recently opened display

--- extensions/vuengine-studio-extension/src/getting-started/browser/ves-getting-started-widget.tsx
+++ extensions/vuengine-studio-extension/src/getting-started/browser/ves-getting-started-widget.tsx
@@ -1,26 +1,31 @@
-import { nls } from '@theia/core';
+import { Path, URI, nls } from '@theia/core';
 import { Message, PreferenceService, codicon } from '@theia/core/lib/browser';
 import { WindowService } from '@theia/core/lib/browser/window/window-service';
 import { inject, injectable, postConstruct } from '@theia/core/shared/inversify';
 import { GettingStartedWidget } from '@theia/getting-started/lib/browser/getting-started-widget';
 import { VSXEnvironment } from '@theia/vsx-registry/lib/common/vsx-environment';
 import * as React from 'react';
+import { VesCommonService } from '../../core/browser/ves-common-service';
 import { VesCoreContribution } from '../../core/browser/ves-core-contribution';
 import { VesProjectCommands } from '../../project/browser/ves-project-commands';
+import { VesProjectService } from '../../project/browser/ves-project-service';
 
 @injectable()
 export class VesGettingStartedWidget extends GettingStartedWidget {
 
     @inject(VSXEnvironment)
     protected readonly environment: VSXEnvironment;
-
+    @inject(VesCommonService)
+    protected readonly vesCommonService: VesCommonService;
+    @inject(VesProjectService)
+    protected readonly vesProjectService: VesProjectService;
     @inject(WindowService)
     protected readonly windowService: WindowService;
-
     @inject(PreferenceService)
     protected readonly preferenceService: PreferenceService;
 
     protected recentLimit = 10;
+    protected vesRecentWorkspaces: Array<{ name: string, uri: URI, path: string }> = [];
 
     @postConstruct()
     protected init(): void {
@@ -29,8 +34,9 @@ export class VesGettingStartedWidget extends GettingStartedWidget {
     }
 
     protected async doInit(): Promise<void> {
-        super.doInit();
+        await super.doInit();
         await this.preferenceService.ready;
+        await this.vesGetRecentWorkspaces();
         this.update();
     }
 
@@ -43,6 +49,19 @@ export class VesGettingStartedWidget extends GettingStartedWidget {
     }
 
     protected createNewProject = async () => this.commandRegistry.executeCommand(VesProjectCommands.NEW.id);
+
+    protected async vesGetRecentWorkspaces(): Promise<void> {
+        for (const workspace of this.recentWorkspaces.slice(0, this.recentLimit)) {
+            const uri = new URI(workspace);
+            const pathLabel = this.labelProvider.getLongName(uri);
+            const path = this.vesCommonService.formatPath(this.home ? Path.tildify(pathLabel, this.home) : pathLabel);
+            const name = await this.vesProjectService.getProjectName(uri);
+
+            this.vesRecentWorkspaces.push({
+                name, uri, path
+            });
+        }
+    }
 
     protected render(): React.ReactNode {
         return <>
@@ -99,6 +118,51 @@ export class VesGettingStartedWidget extends GettingStartedWidget {
                     {this.renderVersion()}
                 </span>
             </h1>
+        </div>;
+    }
+
+    protected renderRecentWorkspaces(): React.ReactNode {
+        const content = this.vesRecentWorkspaces.map((item, index) =>
+            <div className='gs-action-container' key={index}>
+                <a
+                    role={'button'}
+                    tabIndex={0}
+                    onClick={() => this.open(item.uri)}
+                    onKeyDown={(e: React.KeyboardEvent) => this.openEnter(e, item.uri)}
+                >
+                    {item.name}
+                </a>
+                <span className='gs-action-details'>
+                    {item.uri.path.fsPath()}
+                </span>
+            </div>
+        );
+
+        const more = this.vesRecentWorkspaces.length > this.recentLimit && <div className='gs-action-container'>
+            <a
+                role={'button'}
+                tabIndex={0}
+                onClick={this.doOpenRecentWorkspace}
+                onKeyDown={this.doOpenRecentWorkspaceEnter}>
+                {nls.localizeByDefault('More...')}
+            </a>
+        </div>;
+        return <div className='gs-section'>
+            <h3 className='gs-section-header'>
+                <i className={codicon('history')}></i>{nls.localizeByDefault('Recent')}
+            </h3>
+            {this.vesRecentWorkspaces.length > 0 ? content : <p className='gs-no-recent'>
+                {nls.localize('vuengine/gettingStarted/noRecentlyOpenedProjects', 'You have no recently opened projects,') + ' '}
+                <a
+                    role={'button'}
+                    tabIndex={0}
+                    onClick={this.doOpenWorkspace}
+                    onKeyDown={this.doOpenWorkspaceEnter}>
+                    {nls.localize('vuengine/gettingStarted/openAProject', 'open a project')}
+                </a>
+                {' ' + nls.localizeByDefault('to start.')}
+            </p>}
+            {more}
         </div>;
     }
 
