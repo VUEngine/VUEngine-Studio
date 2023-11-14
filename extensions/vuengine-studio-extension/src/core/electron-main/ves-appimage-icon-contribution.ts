c48b74f2ca3dba8199ffd7a7ff26eb23b904e3b4
Christian Radke <c.radke@posteo.de>
2023-11-14 20:23:41 +0100

    change icon

--- extensions/vuengine-studio-extension/src/core/electron-main/ves-appimage-icon-contribution.ts
+++ extensions/vuengine-studio-extension/src/core/electron-main/ves-appimage-icon-contribution.ts
@@ -11,7 +11,7 @@ export class VesAppImageIconContribution implements ElectronMainApplicationContr
             if (windowOptions && windowOptions.icon === undefined) {
                 // The window image is undefined. If the executable has an image set, this is used as a fallback.
                 // Since AppImage does not support this anymore via electron-builder, set an image for the linux platform.
-                windowOptions.icon = path.join(__dirname, '../../resources/icons/512x512.png');
+                windowOptions.icon = path.join(__dirname, '../../resources/icons/MacLauncherIcon/512-512.icns');
             }
         }
     }
