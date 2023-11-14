d219160c06f9fbace9a2f1c72f96673369a80f3b
KR155E <c.radke@posteo.de>
2023-11-14 19:22:22 +0100

    Bring back AppImage target

--- extensions/vuengine-studio-extension/src/core/electron-main/ves-electron-main-application-module.ts
+++ extensions/vuengine-studio-extension/src/core/electron-main/ves-electron-main-application-module.ts
@@ -1,8 +1,12 @@
 import { ContainerModule } from 'inversify';
 import { VesMainApi } from './ves-electron-main-api';
 import { ElectronMainApplicationContribution } from '@theia/core/lib/electron-main/electron-main-application';
+import { VesAppImageIconContribution } from './ves-appimage-icon-contribution';
 
 export default new ContainerModule(bind => {
     bind(VesMainApi).toSelf().inSingletonScope();
     bind(ElectronMainApplicationContribution).toService(VesMainApi);
+
+    bind(VesAppImageIconContribution).toSelf().inSingletonScope();
+    bind(ElectronMainApplicationContribution).toService(VesAppImageIconContribution);
 });
