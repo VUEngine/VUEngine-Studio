f1c484e2770a7e84e4810dab797e53e423244094
Christian Radke <c.radke@posteo.de>
2024-01-26 16:10:06 +0100

    Update Theia to 1.46.0

--- extensions/vuengine-studio-extension/src/core/node/ves-core-variables-server.ts
+++ extensions/vuengine-studio-extension/src/core/node/ves-core-variables-server.ts
@@ -1,5 +1,5 @@
 import { EnvVariablesServerImpl } from '@theia/core/lib/node/env-variables';
-import { FileUri } from '@theia/core/lib/node/file-uri';
+import { FileUri } from '@theia/core/lib/common/file-uri';
 import { injectable } from '@theia/core/shared/inversify';
 import { homedir } from 'os';
 import { join } from 'path';
