ad361c57bb6bffa96f9e66ac7ce2d15fd9d2c783
Christian Radke <c.radke@posteo.de>
2024-04-26 14:03:20 +0200

    Import secondary window styles

---
+++ extensions/vuengine-studio-extension/src/core/browser/ves-core-secondary-window-styles.ts
@@ -0,0 +1,4 @@
+// for import into secondary window
+import '../../../src/core/browser/style/index.css';
+import '../../../src/editors/browser/style/index.css';
+import '../../../src/emulator/browser/style/index.css';
