3acecf9ceb9b44071741346bda8b60537a9dd7ea
Christian Radke <c.radke@posteo.de>
2024-08-01 18:07:53 +0200

    Port back PNG parsing to fix font import

--- extensions/vuengine-studio-extension/src/editors/browser/components/FontEditor/Tools/ImportExport.tsx
+++ extensions/vuengine-studio-extension/src/editors/browser/components/FontEditor/Tools/ImportExport.tsx
@@ -1,8 +1,16 @@
 import { nls } from '@theia/core';
 import { OpenFileDialogProps } from '@theia/filesystem/lib/browser';
+import { FileContent } from '@theia/filesystem/lib/common/files';
 import React, { useContext } from 'react';
 import { EditorsContext, EditorsContextType } from '../../../ves-editors-types';
 import { Size } from '../FontEditorTypes';
+
+export interface ParsedImageData {
+    height: number
+    width: number
+    colorType: number
+    pixelData: number[][]
+};
 
 interface ImportExportProps {
     setCharacters: (characters: number[][][]) => void
@@ -13,8 +21,49 @@ interface ImportExportProps {
 
 export default function ImportExport(props: ImportExportProps): React.JSX.Element {
     const { fileUri, services } = useContext(EditorsContext) as EditorsContextType;
-    /*
-    const imageDataToCharacters = (imageData: ImageData): number[][][] => {
+
+    const parseIndexedPng = async (fileContent: FileContent): Promise<ParsedImageData | false> => {
+        console.log('parseIndexedPng');
+        const PNG = require('@camoto/pngjs/browser').PNG;
+        let imageData: ParsedImageData | false = false;
+
+        await new Promise<void>((resolve, reject) => {
+            new PNG({
+                keepIndexed: true,
+            }).parse(fileContent.value.buffer, (error: unknown, data: unknown): void => {
+                if (error) {
+                    console.error('Error while parsing PNG', error, data);
+                    resolve();
+                }
+            }).on('parsed', function (): void {
+                // @ts-ignore: suppress implicit any errors
+                const png = this;
+                console.log('png', png);
+
+                const height = png.height;
+                const width = png.width;
+                const colorType = png._parser._parser._colorType;
+
+                const pixelData: number[][] = [];
+                [...Array(height)].map((h, y) => {
+                    const line: number[] = [];
+                    [...Array(width)].map((w, x) => {
+                        line.push(png.data[y * width + x]);
+                    });
+                    pixelData.push(line);
+                });
+
+                imageData = { height, width, colorType, pixelData };
+
+                resolve();
+            });
+        });
+
+        console.log('imageData', imageData);
+        return imageData;
+    };
+
+    const imageDataToCharacters = (imageData: ParsedImageData): number[][][] => {
         const charactersPerLine = imageData.width / (props.size.x * 8);
 
         const characters: number[][][] = [];
@@ -44,7 +93,6 @@ export default function ImportExport(props: ImportExportProps): React.JSX.Elemen
 
         return characters;
     };
-    */
 
     const selectImageFile = async (): Promise<void> => {
         const openFileDialogProps: OpenFileDialogProps = {
@@ -56,9 +104,10 @@ export default function ImportExport(props: ImportExportProps): React.JSX.Elemen
         const currentPath = await services.fileService.resolve(fileUri);
         const uri = await services.fileDialogService.showOpenDialog(openFileDialogProps, currentPath);
         if (uri) {
-            /*
+            console.log('uri', uri);
             const fileContent = await services.fileService.readFile(uri);
-            const imgData = await window.electronVesCore.parsePng(fileContent);
+            console.log('fileContent', fileContent);
+            const imgData = await parseIndexedPng(fileContent);
             if (imgData !== false) {
                 props.setCharacters(imageDataToCharacters(imgData));
             } else {
@@ -66,7 +115,6 @@ export default function ImportExport(props: ImportExportProps): React.JSX.Elemen
                     nls.localize('vuengine/fontEditor/errorImporting', 'There was as error importing the PNG file.')
                 );
             }
-            */
         }
     };
 
