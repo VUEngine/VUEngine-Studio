c487dd2ca853103bd01773b201b029f5a0582475
Christian Radke <c.radke@posteo.de>
2024-08-22 10:17:06 +0200

    Tweak sidebar styles

--- extensions/vuengine-studio-extension/src/core/browser/ves-core-color-contribution.ts
+++ extensions/vuengine-studio-extension/src/core/browser/ves-core-color-contribution.ts
@@ -4,8 +4,20 @@ import { injectable } from '@theia/core/shared/inversify';
 
 @injectable()
 export class VesColorContribution implements ColorContribution {
-  registerColors(colors: ColorRegistry): void {
-    colors.register(
+  registerColors(colorRegistry: ColorRegistry): void {
+    colorRegistry.register(
+      {
+        id: 'activityBar.hoverBackground',
+        defaults: {
+          dark: '#2a2d2e',
+          light: '#e8e8e8',
+          hc: '#00000000'
+        },
+        description: 'Background color of activity bar items on hover'
+      }
+    );
+
+    colorRegistry.register(
       {
         id: 'editorSuccess-foreground',
         defaults: {
