acd3e325e202d4a026a552aeb3fb664f651b1a43
Christian Radke <c.radke@posteo.de>
2025-02-05 15:58:28 +0100

    Redesign rotation inputs

--- extensions/vuengine-studio-extension/src/editors/browser/components/Common/Rotation.tsx
+++ extensions/vuengine-studio-extension/src/editors/browser/components/Common/Rotation.tsx
@@ -1,8 +1,10 @@
 import { nls } from '@theia/core';
-import React, { PropsWithChildren, useEffect, useState } from 'react';
+import React, { PropsWithChildren, useContext, useEffect, useState } from 'react';
+import { EditorsContext, EditorsContextType } from '../../ves-editors-types';
+import { INPUT_BLOCKING_COMMANDS } from '../ActorEditor/ActorEditor';
 import HContainer from './Base/HContainer';
-import { clamp } from './Utils';
 import VContainer from './Base/VContainer';
+import { clamp } from './Utils';
 import { MAX_ROTATION, MIN_ROTATION, PixelRotation, ROTATION_RATIO } from './VUEngineTypes';
 
 interface RotationProps {
@@ -12,25 +14,23 @@ interface RotationProps {
 
 export default function Rotation(props: PropsWithChildren<RotationProps>): React.JSX.Element {
     const { rotation, updateRotation } = props;
+    const { enableCommands, disableCommands } = useContext(EditorsContext) as EditorsContextType;
     const [rotationDegrees, setRotationDegrees] = useState<PixelRotation>({ x: 0, y: 0, z: 0 });
 
     const toDegrees = (engineRotation: number): number =>
-        engineRotation * ROTATION_RATIO;
-
-    const toEngineRotation = (degrees: number): number =>
-        Math.round(degrees / ROTATION_RATIO);
+        Math.round(engineRotation * ROTATION_RATIO * 100) / 100;
 
     const setRotation = (a: 'x' | 'y' | 'z', value: number): void => {
-        const engineRotation = clamp(toEngineRotation(value), MIN_ROTATION, MAX_ROTATION);
+        const clampedValue = clamp(value, MIN_ROTATION, MAX_ROTATION);
 
         setRotationDegrees({
             ...rotationDegrees,
-            [a]: toDegrees(engineRotation),
+            [a]: toDegrees(clampedValue),
         });
 
         updateRotation({
             ...rotation,
-            [a]: engineRotation,
+            [a]: clampedValue,
         });
     };
 
@@ -42,50 +42,61 @@ export default function Rotation(props: PropsWithChildren<RotationProps>): React
         });
     }, []);
 
-    return <VContainer>
-        <label>
-            {nls.localize('vuengine/editors/rotation', 'Rotation (x, y, z)')}
-        </label>
-        <HContainer wrap='wrap'>
-            <HContainer gap={2}>
-                <input
-                    className='theia-input'
-                    style={{ width: 76 }}
-                    type='number'
-                    min={MIN_ROTATION}
-                    max={MAX_ROTATION}
-                    step={ROTATION_RATIO}
-                    value={rotationDegrees.x}
-                    onChange={e => setRotation('x', e.target.value === '' ? 0 : parseFloat(e.target.value))}
-                />
-                °
+    return (
+        <VContainer>
+            <label>
+                {nls.localize('vuengine/editors/rotation', 'Rotation (x, y, z)')}
+            </label>
+            <HContainer wrap='wrap'>
+                <VContainer>
+                    <input
+                        className='theia-input'
+                        style={{ width: 76 }}
+                        type='number'
+                        min={MIN_ROTATION}
+                        max={MAX_ROTATION}
+                        value={rotation.x}
+                        onChange={e => setRotation('x', e.target.value === '' ? 0 : parseFloat(e.target.value))}
+                        onFocus={() => disableCommands(INPUT_BLOCKING_COMMANDS)}
+                        onBlur={() => enableCommands(INPUT_BLOCKING_COMMANDS)}
+                    />
+                    <div className="secondaryText">
+                        {rotationDegrees.x}°
+                    </div>
+                </VContainer>
+                <VContainer>
+                    <input
+                        className='theia-input'
+                        style={{ width: 76 }}
+                        type='number'
+                        min={MIN_ROTATION}
+                        max={MAX_ROTATION}
+                        value={rotation.y}
+                        onChange={e => setRotation('y', e.target.value === '' ? 0 : parseFloat(e.target.value))}
+                        onFocus={() => disableCommands(INPUT_BLOCKING_COMMANDS)}
+                        onBlur={() => enableCommands(INPUT_BLOCKING_COMMANDS)}
+                    />
+                    <div className="secondaryText">
+                        {rotationDegrees.y}°
+                    </div>
+                </VContainer>
+                <VContainer>
+                    <input
+                        className='theia-input'
+                        style={{ width: 76 }}
+                        type='number'
+                        min={MIN_ROTATION}
+                        max={MAX_ROTATION}
+                        value={rotation.z}
+                        onChange={e => setRotation('z', e.target.value === '' ? 0 : parseFloat(e.target.value))}
+                        onFocus={() => disableCommands(INPUT_BLOCKING_COMMANDS)}
+                        onBlur={() => enableCommands(INPUT_BLOCKING_COMMANDS)}
+                    />
+                    <div className="secondaryText">
+                        {rotationDegrees.z}°
+                    </div>
+                </VContainer>
             </HContainer>
-            <HContainer gap={2}>
-                <input
-                    className='theia-input'
-                    style={{ width: 76 }}
-                    type='number'
-                    min={MIN_ROTATION}
-                    max={MAX_ROTATION}
-                    step={ROTATION_RATIO}
-                    value={rotationDegrees.y}
-                    onChange={e => setRotation('y', e.target.value === '' ? 0 : parseFloat(e.target.value))}
-                />
-                °
-            </HContainer>
-            <HContainer gap={2}>
-                <input
-                    className='theia-input'
-                    style={{ width: 76 }}
-                    type='number'
-                    min={MIN_ROTATION}
-                    max={MAX_ROTATION}
-                    step={ROTATION_RATIO}
-                    value={rotationDegrees.z}
-                    onChange={e => setRotation('z', e.target.value === '' ? 0 : parseFloat(e.target.value))}
-                />
-                °
-            </HContainer>
-        </HContainer>
-    </VContainer>;
+        </VContainer>
+    );
 }
