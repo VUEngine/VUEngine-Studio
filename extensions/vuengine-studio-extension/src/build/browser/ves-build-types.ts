372017b021f336e14cd830ca17f28b432c0f1c1e
Christian Radke <c.radke@posteo.de>
2026-02-24 23:48:23 +0100

    Slashes, slashes, popeshes

--- extensions/vuengine-studio-extension/src/build/browser/ves-build-types.ts
+++ extensions/vuengine-studio-extension/src/build/browser/ves-build-types.ts
@@ -84,7 +84,7 @@ export enum PrePostBuildTaskType {
   Command = 'command',
 }
 
-export const WSL_ENGINE_CORE_PATH = '~/vuengine/core';
-export const WSL_ENGINE_PLUGINS_PATH = '~/vuengine/plugins';
-export const WSL_USER_PLUGINS_PATH = '~/vuengine/user';
-export const WSL_PROJECTS_PATH = '~/vuengine/projects';
+export const WSL_ENGINE_CORE_PATH = '~/vuengine/core/';
+export const WSL_ENGINE_PLUGINS_PATH = '~/vuengine/plugins/';
+export const WSL_USER_PLUGINS_PATH = '~/vuengine/user/';
+export const WSL_PROJECTS_PATH = '~/vuengine/projects/';
