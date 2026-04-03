5836c295a31589752ec70d2d0924a7f3dd5c54b1
Christian Radke <c.radke@posteo.de>
2026-04-03 10:39:33 +0200

    Pin settings widgets in settings view mode

--- extensions/vuengine-studio-extension/src/viewMode/browser/view-mode-frontend-module.ts
+++ extensions/vuengine-studio-extension/src/viewMode/browser/view-mode-frontend-module.ts
@@ -2,8 +2,11 @@ import { KeybindingContribution, ShellLayoutRestorer, ShellLayoutTransformer } f
 import { CommandContribution } from '@theia/core/lib/common/command';
 import { MenuContribution } from '@theia/core/lib/common/menu';
 import { ContainerModule } from '@theia/core/shared/inversify';
+import { PreferencesWidget } from '@theia/preferences/lib/browser/views/preference-widget';
+import { createPreferencesWidgetContainer } from '@theia/preferences/lib/browser/views/preference-widget-bindings';
 import '../../../src/viewMode/browser/style/index.css';
 import { ViewModeContribution } from './view-mode-contribution';
+import { ViewModePreferencesWidget } from './view-mode-preferences-widget';
 import { ViewModeService } from './view-mode-service';
 import { ViewModeShellLayoutRestorer } from './view-mode-shell-layout-restorer';
 import { ViewModeShellLayoutTransformer } from './view-mode-shell-layout-transformer';
@@ -18,4 +21,9 @@ export default new ContainerModule((bind, unbind, isBound, rebind) => {
 
     rebind(ShellLayoutRestorer).to(ViewModeShellLayoutRestorer).inSingletonScope();
     bind(ShellLayoutTransformer).to(ViewModeShellLayoutTransformer);
+
+    bind(ViewModePreferencesWidget).toSelf().inSingletonScope();
+    rebind(PreferencesWidget)
+        .toDynamicValue(({ container }) => createPreferencesWidgetContainer(container).get(ViewModePreferencesWidget))
+        .inSingletonScope();
 });
