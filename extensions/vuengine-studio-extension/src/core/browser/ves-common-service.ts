ecb692430a05a813472b344a7bee3c971c0d6c6e
Christian Radke <c.radke@posteo.de>
2026-07-31 18:31:22 +0200

    Update to Theia 1.74.0

--- extensions/vuengine-studio-extension/src/core/browser/ves-common-service.ts
+++ extensions/vuengine-studio-extension/src/core/browser/ves-common-service.ts
@@ -78,7 +78,7 @@ export class VesCommonService {
       : ';';
   }
 
-  base64ToBytes(base64: string): Uint8Array {
+  base64ToBytes(base64: string): Uint8Array<ArrayBuffer> {
     const binString = atob(base64);
     return Uint8Array.from(binString, m => m.codePointAt(0) ?? 0);
   }
