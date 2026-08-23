a6baecb42c803fee1d7e5aa10162c6459204794b
Christian Radke <c.radke@posteo.de>
2026-08-23 02:28:10 +0200

    Implement proper "open with" support

--- extensions/vuengine-studio-extension/src/touchbar/electron-main/ves-touchbar-electron-main-application-module.ts
+++ extensions/vuengine-studio-extension/src/touchbar/electron-main/ves-touchbar-electron-main-application-module.ts
@@ -1,7 +1,7 @@
 import { ContainerModule } from '@theia/core/shared/inversify';
 import { ElectronMainApplication } from '@theia/core/lib/electron-main/electron-main-application';
-import { VesElectronMainApplication } from './ves-touchbar-electron-main-application';
+import { VesTouchbarElectronMainApplication } from './ves-touchbar-electron-main-application';
 
 export default new ContainerModule((bind, unbind, isBound, rebind) => {
-    rebind(ElectronMainApplication).to(VesElectronMainApplication).inSingletonScope();
+    rebind(ElectronMainApplication).to(VesTouchbarElectronMainApplication).inSingletonScope();
 });
